ba23a04466925c19136c59f9ce8f88e476c502c3
Ayush Sinha <ayushsinha353@gmail.com>
2020-06-24 11:47:57 +0530

    added add.py file for adding an addition program to our project

---
+++ add.py
@@ -0,0 +1,12 @@
+#This program will add two numbers 
+
+#Taking input from the user
+
+x=int(input("Enter the value of X:"))
+y=int(input("Enter the value of Y:"))
+
+#logic:
+z=x+y
+
+#output:
+print(f"Your Answer is: {z} ")
